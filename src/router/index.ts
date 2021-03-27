5ae4af49b63ee7ef21979a41223ef7e3f17f322c
gamin27 <nagamineyusuke17@gmail.com>
2021-03-27 14:03:21 +0900

    コメントを削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,9 +11,6 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     // todo: aboutの書式を合わせる
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
