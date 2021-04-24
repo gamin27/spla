4485085b9d3ae43a627a588dabd39a2e5c4db6a7
gamin27 <nagamineyusuke17@gmail.com>
2021-04-24 22:43:37 +0900

    amplifyを追加してみた

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
+import router from './router/index';
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-    <router-link to="/v-model-test-page">V-model</router-link> |
-    <router-link to="/apollo-test-page">Apollo</router-link>
+    <ul class="router-links">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/v-model-test-page">V-model</router-link></li>
+      <li><router-link to="/apollo-test-page">Apollo</router-link></li>
+      <li><router-link to="/amplify-test-page">Amplify</router-link></li>
+    </ul>
   </div>
   <router-view />
 </template>
 
-<style>
+<style lang="scss" scoded>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +32,14 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.router-links {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  li {
+    margin-right: 16px;
+  }
 }
 </style>
