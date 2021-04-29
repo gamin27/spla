86714a444c4adc67fcd2e3fa0e6f4017f090dc49
gamin27 <nagamineyusuke17@gmail.com>
2021-04-29 18:00:49 +0900

    ファイル名を変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/Home.vue'
-import WinningPercentage from '@/views/WinningPercentage.vue'
-import About from '@/views/About.vue'
+import Home from '@/views/home/index.vue'
+import WinningPercentage from '@/views/winning/index.vue'
+import About from '@/views/about/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
