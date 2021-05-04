8cac058685012922af174d6f0fce7dfc03449b9e
gamin27 <nagamineyusuke17@gmail.com>
2021-05-04 21:19:27 +0900

    titleを変えた

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,19 @@ module.exports = {
       .loader('graphql-tag/loader')
       .end()
   },
+  pwa: {
+    name: 'Spla AI',
+    // themeColor: '#4DBA87',
+    // msTileColor: '#000000',
+    // appleMobileWebAppCapable: 'yes',
+    // appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    // swSrc is required in InjectManifest mode.
+    // swSrc: 'dev/sw.js',
+    // ...other Workbox options...
+    // },
+  },
 }
