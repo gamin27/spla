88cff8f94d2e5e990fffb2ecdf61e5bbd6f42aa2
gamin27 <nagamineyusuke17@gmail.com>
2021-05-08 14:13:11 +0900

    スケジュール表示

--- src/views/schedule/schedule.vue
+++ src/views/schedule/schedule.vue
@@ -1,50 +1,81 @@
 <template>
   <h1>Schedule</h1>
-  <section class="rule">
-    <div class="rule__label">Rule</div>
-    <div class="rule__name">
-      <div v-if="stage.rule">
-        <div>{{ stage.rule }}</div>
-      </div>
-      <div v-else><Skeleton :borderRadius="bprderRadius" height="24px" /></div>
+  <div v-if="stages[0]">
+    <article v-for="stage in stages" :key="stage.start">
+      <section class="time section">
+        <div v-if="stage.start">{{ time(stage.start) }} ~ {{ time(stage.end) }}</div>
+        <div v-else><Skeleton :borderRadius="bprderRadius" height="18px" width="180px" style="margin: auto" /></div>
+      </section>
+      <section class="rule section">
+        <div class="rule__label">Rule</div>
+        <div class="rule__name">
+          <div v-if="stage.rule">
+            <div>{{ stage.rule }}</div>
+          </div>
+          <div v-else><Skeleton :borderRadius="bprderRadius" height="24px" width="240px" style="margin: auto" /></div>
+        </div>
+      </section>
+      <section class="stage section">
+        <div class="stage__rule">Stage</div>
+        <ul v-if="stage.maps_ex">
+          <li v-for="map in stage.maps_ex" :key="map.id" class="stage__item">
+            <div class="item__name">
+              <span v-if="stage.maps_ex">
+                {{ map.name }}
+              </span>
+            </div>
+            <div class="item__image">
+              <div v-show="isLoading">
+                <Skeleton :borderRadius="bprderRadius" height="96px" />
+              </div>
+              <div v-show="!isLoading">
+                <img :src="map.image" @load="isLoaded" alt="map.id" />
+              </div>
+            </div>
+          </li>
+        </ul>
+        <ul v-else>
+          <li v-for="(item, index) in 2" :key="index" class="stage__item">
+            <div class="item__name">
+              <Skeleton :borderRadius="bprderRadius" width="180px" height="20px" style="margin: auto" />
+            </div>
+            <div class="item__image">
+              <Skeleton :borderRadius="bprderRadius" height="96px" />
+            </div>
+          </li>
+        </ul>
+      </section>
+    </article>
+  </div>
+  <div v-else>
+    <div v-for="(item, index) in 4" :key="index">
+      <section class="time section">
+        <Skeleton :borderRadius="bprderRadius" height="18px" width="180px" style="margin: auto" />
+      </section>
+      <section class="rule section">
+        <div class="rule__label">Rule</div>
+        <div class="rule__name">
+          <Skeleton :borderRadius="bprderRadius" height="24px" width="240px" style="margin: auto" />
+        </div>
+      </section>
+      <section class="stage section">
+        <div class="stage__rule">Stage</div>
+        <ul>
+          <li v-for="(item, index) in 2" :key="index" class="stage__item">
+            <div class="item__name">
+              <Skeleton :borderRadius="bprderRadius" width="180px" height="20px" style="margin: auto" />
+            </div>
+            <div class="item__image">
+              <Skeleton :borderRadius="bprderRadius" height="96px" />
+            </div>
+          </li>
+        </ul>
+      </section>
     </div>
-  </section>
-  <section class="stage">
-    <div class="stage__rule">Stage</div>
-    <ul v-if="stage.maps_ex">
-      <li v-for="map in stage.maps_ex" :key="map.id" class="stage__item">
-        <div class="item__name">
-          <span v-if="stage.maps_ex">
-            {{ map.name }}
-          </span>
-          <!-- <span v-else>
-            <Skeleton :borderRadius="bprderRadius" width="100%" />
-          </span> -->
-        </div>
-        <div class="item__image">
-          <div v-show="isLoading">
-            <Skeleton :borderRadius="bprderRadius" height="96px" />
-          </div>
-          <div v-show="!isLoading">
-            <img :src="map.image" @load="isLoaded" alt="map.id" />
-          </div>
-        </div>
-      </li>
-    </ul>
-    <ul v-else>
-      <li v-for="(item, index) in 2" :key="index" class="stage__item">
-        <div class="item__name">
-          <Skeleton :borderRadius="bprderRadius" width="100%" height="32px" />
-        </div>
-        <div class="item__image">
-          <Skeleton :borderRadius="bprderRadius" height="96px" />
-        </div>
-      </li>
-    </ul>
-  </section>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref } from '@vue/runtime-core'
 import Skeleton from 'primevue/skeleton'
 
@@ -55,12 +86,11 @@ export default defineComponent({
   },
   setup() {
     const bprderRadius = '4px'
-    const stage = ref({})
+    const stages = ref([])
     const isLoading = ref(true)
-    const isLoaded = () => {
-      isLoading.value = false
-    }
-    fetch('https://spla2.yuu26.com/gachi/now', {
+    const isLoaded = () => (isLoading.value = false)
+
+    fetch('https://spla2.yuu26.com/gachi/schedule', {
       method: 'GET',
       mode: 'cors',
     })
@@ -68,25 +98,36 @@ export default defineComponent({
         return response.json()
       })
       .then((result) => {
-        stage.value = result.result[0]
+        stages.value = result.result
+        console.log('---result', result)
       })
       .catch((error) => console.log('error', error))
 
     return {
-      stage,
+      stages,
       bprderRadius,
       isLoading,
       isLoaded,
+      time: (value: string) => value?.slice(11, -3) || '',
     }
   },
 })
 </script>
 
 <style lang="scss" scoped>
+$main-color: #80c683;
+.section {
+  margin: 16px auto 0;
+}
+.time {
+  font-weight: 600;
+  color: $main-color;
+  font-size: 18px;
+}
 .rule {
-  margin: 32px auto 0;
   .rule__label {
     font-weight: 600;
+    color: $main-color;
   }
   .rule__name {
     margin: 8px auto 0;
@@ -95,26 +136,26 @@ export default defineComponent({
   }
 }
 .stage {
-  margin-top: 32px;
   .stage__rule {
     font-weight: 600;
+    color: $main-color;
   }
   .stage__item {
-    margin: 16px auto 0;
+    margin: 8px auto 0;
   }
   .item__name {
     font-weight: 600;
-    font-size: 24px;
-    height: 32px;
+    font-size: 20px;
   }
   .item__image {
-    margin: 0 auto;
+    margin: 4px auto 0;
     width: 100vw;
     height: 96px;
     img {
       width: 100vw;
       height: 96px;
       object-fit: cover;
+      filter: brightness(60%);
     }
   }
 }
